46293d6470ade4fe70dbc12426103e20d3a24bde
rosa ovalle <you@example.com>
2024-10-30 22:35:47 -0700

    first commit

---
+++ lecact3.js
@@ -0,0 +1,15 @@
+function greeting(name){
+    console.log("Hey, " + name);
+}
+
+greeting("You!");
+
+
+
+//#2 
+let diameter = 15;
+if ( diameter > 0) {
+    let pi = 3.14;
+    let circumference = pi * diameter;
+    console.log ("circumference= " + circumference);
+}
